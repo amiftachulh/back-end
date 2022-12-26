10a075f823a5a1a3cf769cd76524b51b9f04ca2c
amiftachulh <81857387+amiftachulh@users.noreply.github.com>
2022-12-26 21:27:05 +0700

    Update user.schema.ts

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -4,14 +4,12 @@ export const userCreate = z.object({
   username: z.string().min(4).max(30),
   password: z.string().min(8).max(32),
   email: z.string().email().max(254),
-  role: z.string(),
 });
 
 export const userUpdate = z.object({
   username: z.string().min(4).max(30).optional(),
   password: z.string().min(8).max(32).optional(),
   email: z.string().email().max(254).optional(),
-  role: z.string().optional(),
 });
 
 export const userReturn = userUpdate.omit({ password: true });
